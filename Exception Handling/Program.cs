b84a80078c860231845c9e100a12bba03faf2924
oliviaalchemy <olivia@alchemytechs.com>
2019-08-08 09:23:50 +0100

    Add project files.

---
+++ Exception Handling/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 / num2);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("e.Message");
+            }    
+
+
+            Console.ReadLine(); //will print out error
+        }
+    }
+}
